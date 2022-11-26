fdeb8d214ec8924e7765e4225d0f00054c75f2db
GzINOD <gzinod31@rambler.ru>
2022-11-26 14:40:02 +0300

    Homework 29 done

---
+++ HomeTask27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+System.Console.WriteLine("Num");
+var num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumm(int (num))
+{
+     
+}
