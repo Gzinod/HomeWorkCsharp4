a91de201c9eef2533b47a95c615f55570e5909d1
GzINOD <gzinod31@rambler.ru>
2022-11-26 15:54:25 +0300

    HomeWork Done

--- HomeTask27/Program.cs
+++ HomeTask27/Program.cs
@@ -8,9 +8,60 @@
 
 System.Console.WriteLine("Num");
 var num = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [GetLength(num)];
+System.Console.WriteLine(array.Length);
+var count = GetLength(num);
 
 int GetLength(int num)
 {
-     for(int i=0;num%10!=0;i++)
+    var i = 0;
+     for( i=0;num!=0;i++)
+    {
+        num/=10;
+    }
      return i;
 }
+
+if (count == 1)
+{
+    Console.WriteLine(num);
+}
+else
+    {
+        if (count == 2)
+        {
+            int num1 = num / 10;
+            int num2 = num % 10;
+            int summ = num1 + num2;
+            System.Console.WriteLine(summ);
+        }
+         else
+        {
+            if(count == 3)
+            {
+                int num1 = num / 100;
+                int num2 = num % 100 / 10;
+                int num3 = num % 10;
+                int summ = num1 + num2 + num3;
+                System.Console.WriteLine(summ);
+            }
+            else
+            {
+                if(count == 4)
+                {
+                    int num1 = num / 1000;
+                    int num2 = num % 1000 / 100;
+                    int num3 = num % 100 / 10;
+                    int num4 = num % 10;
+                    int summ = num1 + num2 + num3 + num4;
+                    System.Console.WriteLine(summ);
+                }
+                
+            }
+        }
+    }
+
+        
+   
+    
+    
